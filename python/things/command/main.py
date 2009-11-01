1944cad0a1027cf7fee8d6a448ddee1565cdd7be
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2009-11-01 16:45:17 +0000

    * python/things/command/main.py: 	  Colorize output like yagtd, except we use prioritized color 	  according to the priority.

--- python/things/command/main.py
+++ python/things/command/main.py
@@ -24,6 +24,62 @@ When adding or searching for things, the following syntax is used:
  - E:YYYY-MM-HH end date
 
 """
+# Colorization
+COLOR_CODES = ( { 'none': "",
+                  'default': "\033[0m",
+                  # primary colors
+                  'black': "\033[0;30m",
+                  'grey': "\033[0;37m",
+                  'red': "\033[0;31m",
+                  'green': "\033[0;32m",
+                  'blue': "\033[0;34m",
+                  'purple': "\033[0;35m",
+                  'cyan': "\033[0;36m",
+                  'yellow': "\033[0;33m",
+                  # bold colors
+                  'white': "\033[1;37m",
+                  'dark_grey': "\033[1;30m",
+                  'dark_red': "\033[1;31m",
+                  'dark_green': "\033[1;32m",
+                  'dark_blue': "\033[1;34m",
+                  'dark_purple': "\033[1;35m",
+                  'dark_cyan': "\033[1;36m",
+                  'dark_yellow': "\033[1;33m",
+                  # other colors                  
+                  'normal': "\x1b[0;37;40m",
+                  'title': "\x1b[1;32;40m",
+                  'heading': "\x1b[1;35;40m",
+                  'bold': "\x1b[1;35;40m",
+                  'important': "\x1b[1;31;40m",
+                  'error': "\x1b[1;31;40m",
+                  'reverse': "\x1b[0;7m",
+                  'row0': "\x1b[0;35;40m",
+                  'row1': "\x1b[0;36;40m" } )
+
+# Default colors
+DEFAULT_COLOR    = COLOR_CODES['default']
+CONTEXT_COLOR    = COLOR_CODES['dark_yellow']
+PROJECT_COLOR    = COLOR_CODES['dark_purple']
+STATUS_COLOR     = COLOR_CODES['dark_green']
+REFERENCE_COLOR  = COLOR_CODES['dark_blue']
+URGENCY_COLOR    = COLOR_CODES['red']
+IMPORTANCE_COLOR = COLOR_CODES['red']
+COMPLETE_COLOR   = COLOR_CODES['white']
+TIME_COLOR       = COLOR_CODES['cyan']
+RECURRENCE_COLOR = COLOR_CODES['cyan']
+START_COLOR      = COLOR_CODES['red']
+DUE_COLOR        = COLOR_CODES['red']
+END_COLOR        = COLOR_CODES['green']
+
+# priority colors; from 0 to 5
+P_COLORS = [
+  COLOR_CODES['dark_green'],
+  COLOR_CODES['yellow'],
+  COLOR_CODES['dark_yellow'],
+  COLOR_CODES['red'],
+  COLOR_CODES['dark_red'],
+  COLOR_CODES['dark_purple'],
+]
 
 def main(argv):
     c = GTD()
@@ -43,35 +99,57 @@ def main(argv):
 
     return ret
 
-def display(thing):
-    # FIXME: colorize
+def display(thing, colored=True):
+    def color(text, code):
+        if not colored:
+            return text
+
+        return code + text + DEFAULT_COLOR
+
+    def pcolor(text, priority):
+        # color according to priority
+        if not colored:
+            return text
+
+        return P_COLORS[int(priority)] + text + DEFAULT_COLOR
+
     blocks = []
-    blocks.append('%s:(%.2f)' % (thing.shortid(), thing.priority()))
+    blocks.append(color('%s' % thing.shortid(), TIME_COLOR))
+    blocks.append(pcolor('(%.2f)' % thing.priority(), thing.priority()))
     blocks.append(thing.title)
 
     if thing.contexts:
-        blocks.extend(['@%s' % c for c in thing.contexts]) 
+        blocks.extend([color('@%s' % c, CONTEXT_COLOR)
+            for c in thing.contexts]) 
     if thing.projects:
-        blocks.extend(['p:%s' % p for p in thing.projects]) 
+        blocks.extend([color('p:%s' % p, PROJECT_COLOR)
+            for p in thing.projects]) 
     if thing.statuses:
-        blocks.extend(['!%s' % s for s in thing.statuses]) 
+        blocks.extend([color('!%s' % s, STATUS_COLOR)
+             for s in thing.statuses]) 
 
     if thing.urgency is not None:
-        blocks.append('U:%d' % thing.urgency)
+        blocks.append(color('U:%d', URGENCY_COLOR) % thing.urgency)
     if thing.importance is not None:
-        blocks.append('I:%d' % thing.importance)
+        blocks.append(color('I:%d', IMPORTANCE_COLOR) % thing.importance)
 
     # FIXME: format with H/M/S/...
     if thing.time is not None:
-        blocks.append('T:%d' % thing.time)
+        blocks.append(color(
+            'T:%d' % thing.time, TIME_COLOR))
 
     if thing.start is not None:
-        blocks.append('S:%s' % thing.start.strftime('%Y-%m-%d'))
+        blocks.append(color(
+            'S:%s' % thing.start.strftime('%Y-%m-%d'), START_COLOR))
     if thing.due is not None:
-        blocks.append('D:%s' % thing.due.strftime('%Y-%m-%d'))
+        blocks.append(color(
+            'D:%s' % thing.due.strftime('%Y-%m-%d'), DUE_COLOR))
+    if thing.end is not None:
+        blocks.append(color(
+            'E:%s' % thing.due.strftime('%Y-%m-%d'), END_COLOR))
 
     if thing.complete:
-        blocks.append('C:%s' % thing.complete)
+        blocks.append(color('C:%s' % thing.complete, COMPLETE_COLOR))
 
     return " ".join(blocks)
 
@@ -154,8 +232,10 @@ class Search(logcommand.LogCommand):
         # now apply all filters in a row
         for attribute in ['urgency', 'importance', 'time']:
             if filter.has_key(attribute) and attribute != fattribute:
-                self.debug('filtering on %s: %s' % (attribute, filter[attribute]))
-                result = [t for t in result if str(t[attribute]).find(str(filter[attribute])) > -1]
+                self.debug('filtering on %s: %s' % (
+                    attribute, filter[attribute]))
+                result = [t for t in result 
+                    if str(t[attribute]).find(str(filter[attribute])) > -1]
 
         # separate because filter has singular, Thing has plural
         for attribute in ['projects', 'contexts']:
