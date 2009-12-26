c411c922a8d2801d010e8626fbfb57204b7a6ffe
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2009-12-26 17:42:58 +0000

    * couchdb/shows/thing.js: 	* couchdb/templates/thing.html: 	  Use mustache.  Use a dirty trick to do object property access 	  in templates.

--- couchdb/shows/thing.js
+++ couchdb/shows/thing.js
@@ -3,13 +3,16 @@ function(doc, req) {
   // !json thing
   // !code vendor/couchapp/template.js
   // !code vendor/couchapp/path.js
+  // !code vendor/mustache/mustache.js
 
-  // we only show html
-  return template(templates.thing, {
-    thing : doc,
-    start : doc.start,
-    assets : assetPath(),
-    editThingPath : showPath('edit', doc._id),
-    index : listPath('index','recent-things',{descending:true, limit:5})
-  });
+ // FIXME: using a list to switch context in html to thing feels dirty
+   return Mustache.to_html(templates.thing, {
+     thing: [doc, ],
+     title: doc.title,
+     start: doc.start,
+     due: doc.due,
+     assets : assetPath(),
+     editThingPath : showPath('edit', doc._id),
+     index : listPath('index','recent-things',{descending:true, limit:5})
+   });        
 }
