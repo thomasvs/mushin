fdadae77245a40ae90889afe5c59ddbd372c03cf
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-05-13 16:29:55 +0000

    * bin/mushin-maemo: 	* python/mushin/maemo/main.py: 	  Move pjson mangling to avoid the reactor.

--- python/mushin/maemo/main.py
+++ python/mushin/maemo/main.py
@@ -5,10 +5,14 @@
 import gtk
 
 def start():
+    from mushin.common import log
+    log.debug('main', 'starting')
     gtk.set_application_name('mushin')
 
     from mushin.maemo import start
+    log.debug('main', 'creating window')
     window = start.StartWindow()
+    log.debug('main', 'created window %r', window)
     window.connect('destroy', lambda _: gtk.main_quit())
 
 def main(argv):
@@ -19,6 +23,11 @@ def main(argv):
     from twisted.internet import gtk2reactor
     gtk2reactor.install()
     from twisted.internet import reactor
+
+    # set paisley to non-strict since we only have simplejson on maemo
+    from mushin.extern.paisley import pjson
+
+    assert not pjson.STRICT
 
     log.logTwisted()
 
