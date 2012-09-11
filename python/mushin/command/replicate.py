571fa584c210490ccc620155d562935371654d63
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-09-11 09:42:13 +0000

    * python/mushin/command/replicate.py: 	  Rewrite replicate to use the actual server and Twisted. 	  Handle errors nicer.

--- python/mushin/command/replicate.py
+++ python/mushin/command/replicate.py
@@ -1,25 +1,29 @@
 # -*- Mode: Python -*-
 # vi:si:et:sw=4:sts=4:ts=4
 
-from mushin.common import logcommand
+from twisted.internet import defer
+from twisted.web import error as twerror
+
+from paisley import pjson as json
+
+from mushin.extern.log import log
+
+from mushin.common import logcommand, tcommand
 
 HOST = 'localhost'
 PORT = 5984
 DB = 'mushin'
 
-class Add(logcommand.LogCommand):
+class Add(tcommand.TwistedCommand):
     summary = "Add another database to replicate with"
     usage = "REMOTE_HOST[:REMOTE_PORT][/REMOTE_DB]"
 
-    def do(self, args):
-        import socket
-        import httplib
-        import cjson as json
+    @defer.inlineCallbacks
+    def doLater(self, args):
 
         c = self.getRootCommand()
-        # FIXME: this hardcodes our own port/server
-        conn = httplib.HTTPConnection('%s:%d' % (c.host, c.port))
 
+        # FIXME: parse with a proper library, could also include auth
         try:
             jane = args[0]
         except IndexError:
@@ -45,30 +49,58 @@ class Add(logcommand.LogCommand):
           "http://%s:%d/%s" % (jane, port, db),
         ]
 
+        server = c.getNewServer()
+        # FIXME: don't poke privately
+        client = server._couch
+
         for source, target in [(dbs[0], dbs[1]), (dbs[1], dbs[0])]:
-            s = json.encode({
+            s = json.dumps({
               "source": source,
               "target": target,
               "continuous": True})
+            self.info('replicating from %s to %s', source, target)
             self.debug('json string: %s', s)
             try:
-                conn.request('POST', '/_replicate', s,
-                    {"Content-Type": "application/json"})
-            except socket.error:
-                self.stdout.write('FAILED: local server failed for source %s\n' % source)
+                d = client.post('/_replicate', s)
+            except Exception, e:
+                self.stdout.write('Exception %r\n', e)
+                self.stdout.write(
+                    'FAILED: local server failed for source %s\n' %
+                        source.encode('utf-8'))
                 self.stdout.write('Is the server running ?\n')
-                return
-            try:
-                r = conn.getresponse()
-            except httplib.ResponseNotReady:
-                self.stdout.write('FAILED: local server failed for source %s\n' % source)
+                defer.returnValue(e)
                 return
 
-            if r.status / 100 == 2:
-                self.stdout.write("%r\n" % json.decode(r.read()))
-            else:
-                self.stdout.write('FAILED: status code %r\n' % r.status)
-                return
+            error = None # set with a non-newline string in case of error
+
+            try:
+                result = yield d
+            except twerror.Error, e:
+                error = 'CouchDB returned error response %r' % e.status
+                try:
+                    r = json.loads(e.message)
+                    error = 'CouchDB returned error reason: %s' % r['reason']
+                except:
+                    pass
+            except:
+                error = log.getExceptionMessage(e)
+
+            if not error:
+                r = json.loads(result)
+
+                try:
+                    if r['ok']:
+                        self.stdout.write('+ Replicating %s to %s\n' % (
+                            source.encode('utf-8'), target.encode('utf-8')))
+                    else:
+                        error = r
+                except Exception, e:
+                    error = 'Exception: %r\n' % e
+
+            if error:
+                self.stdout.write('- Failed to replicate %s to %s:\n' % (
+                    source.encode('utf-8'), target.encode('utf-8')))
+                self.stdout.write('  %s\n' % error)
 
 class Replicate(logcommand.LogCommand):
     description = """Manage replication.
