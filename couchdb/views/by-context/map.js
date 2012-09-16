7903e9cab73d8cedcc96a6b6b9ddc24332fed1b3
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2012-09-16 18:28:42 +0200

    gjslint cleanup

--- couchdb/views/by-context/map.js
+++ couchdb/views/by-context/map.js
@@ -1,11 +1,11 @@
 function(doc) {
-  if (doc.type == "thing" && doc.complete != 100 && doc.contexts) {
-    doc.contexts.forEach(function (context) {
+  if (doc.type == 'thing' && doc.complete != 100 && doc.contexts) {
+    doc.contexts.forEach(function(context) {
       emit([context, doc.start], {
-        title : doc.title,
-        start : doc.start,
-        due : doc.due || null
-      });    
+        title: doc.title,
+        start: doc.start,
+        due: doc.due || null
+      });
     });
   }
-};
+}
