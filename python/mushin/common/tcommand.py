7c98def91c3527cc2f8982c7860550b9ec1417a0
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-01-13 21:42:42 +0100

    handle getting ConnectionRefused in our own TCommand

--- python/mushin/common/tcommand.py
+++ python/mushin/common/tcommand.py
@@ -17,13 +17,14 @@ a Command subclass for Twisted-using commands.
 # The Twisted root command will take care of stopping the reactor and returning
 # the exit value.
 
+from twisted.internet import error
 
-from mushin.extern.command import tcommand
+from mushin.extern.command import command, tcommand
 
 from . import logcommand
 
+
 class TwistedCommand(tcommand.TwistedCommand, logcommand.LogCommand):
-    pass
 # FIXME: looks like we don't need to chain up to make this work
 # FIXME: if we do need to chain up again, add info, and make sure we
 #        adjust the logging depth since this chaining adds one level
@@ -32,6 +33,22 @@ class TwistedCommand(tcommand.TwistedCommand, logcommand.LogCommand):
 #        logcommand.LogCommand.debug(self, format, *args)
 #    def warning(self, format, *args):
 #        logcommand.LogCommand.warning(self, format, *args)
+
+    def do(self, args):
+        d = tcommand.TwistedCommand.do(self, args)
+
+        def connectionRefusedEb(failure):
+            self.debug('connection closed, %r', failure)
+            failure.trap(error.ConnectionRefusedError)
+            self.debug('connection refused')
+            msg = 'Could not make a connection to CouchDB.'
+            raise command.CommandError(msg)
+
+
+        d.addErrback(connectionRefusedEb)
+
+        return d
+
 
 class LogReactorCommand(tcommand.ReactorCommand, logcommand.LogCommand):
     pass
